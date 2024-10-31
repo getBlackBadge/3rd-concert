ee0b0997755e039da239f9c884e8b3e2d354d65e
getBlackBadge <gudwls0227@gmail.com>
2024-10-31 16:09:05 +0900

    [feat] postgres 트랜잭션 락 구현 비관적, 낙관적

---
+++ src/common/managers/locks/psql-optimistic-lock.manager.ts
@@ -0,0 +1,83 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { ILockManager } from './interface/lock.manager.interface';
+import { Concert } from '../../../domain/entities/concert.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Queue } from '../../../domain/entities/queue.entity';
+import { User } from '../../../domain/entities/user.entity';
+import { Seat } from '../../../domain/entities/seat.entity';
+import { Reservation } from '../../../domain/entities/reservation.entity';
+
+@Injectable()
+export class OpLockManager implements ILockManager{
+    constructor(    
+        @InjectRepository(Concert)
+        private concertRepository: Repository<Concert>,
+        @InjectRepository(Queue)
+        private queueRepository: Repository<Queue>,
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+        @InjectRepository(Seat)
+        private seatRepository: Repository<Seat>,
+        @InjectRepository(Reservation)
+        private reservationRepository: Repository<Reservation>,
+    
+        private readonly dataSource: DataSource,
+      ) {}
+
+
+    async withLockBySrc<T>(resourceId: string, resourceType: string, operation: (transactionalEntityManager) => Promise<T>): Promise<T> {
+        const queryRunner = this.dataSource.createQueryRunner();
+
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+
+        try {
+            const targetRepo = this.selectRepoByName(resourceType);
+            const lockRepository = queryRunner.manager.withRepository(targetRepo);
+            // concertId에 대해 optimistic 락을 사용하여 동시성 제어
+            const entity = await lockRepository.findOne({ where: { id: resourceId } });
+            const version = entity?.updated_at || new Date(); // Add now if version is undefined
+            const lock = await lockRepository.findOne({
+                where: { id: resourceId },
+                lock: { mode: 'optimistic', version: version },
+            });
+
+            if (!lock) {
+                throw new Error(`resource ${resourceType} with ID ${resourceId} not found`);
+            }
+
+            console.log(`${resourceType} ID ${resourceId}에 대한 락 획득.`);
+
+            // cb
+            const result = await operation(queryRunner.manager);
+
+            await queryRunner.commitTransaction();
+            console.log(`Transaction for ${resourceType} ID ${resourceId} committed successfully.`);
+            return result;
+        } catch (error) {
+            await queryRunner.rollbackTransaction();
+            throw error;
+        } finally {
+            await queryRunner.release();
+        }
+    }
+    
+    private selectRepoByName(name: string): Repository<any> {
+        switch (name) {
+            case 'concert':
+                return this.concertRepository;
+            case 'queue':
+                return this.queueRepository;
+            case 'user':
+                return this.userRepository;
+            case 'seat':
+                return this.seatRepository;
+            case 'reservation':
+                return this.reservationRepository;
+            default:
+                throw new Error(`Repository for ${name} not found`);
+            }
+        }
+        
+        }
