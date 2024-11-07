206cf8d55d07225af6d5ffff1d489ebb2e4ea1b2
getBlackBadge <gudwls0227@gmail.com>
2024-11-08 00:58:31 +0900

    [test] 수정된 대기열 로직 정확히 작동하는지 테스트코드 추가

--- src/application/facades/test/queue.facade.spec.ts
+++ src/application/facades/test/queue.facade.spec.ts
@@ -2,7 +2,6 @@ import { v4 as uuid } from 'uuid';
 import { TestingModule, Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { QueueFacade } from '../queue.facade';
-import { Queue } from '../../../domain/entities/queue.entity';
 import { Concert } from '../../../domain/entities/concert.entity';
 import { User } from '../../../domain/entities/user.entity';
 import { JwtService } from '../../../common/jwt/jwt.service';
@@ -16,6 +15,8 @@ import { RedisRepository } from '../../../infrastructure/redis/redis.repository'
 import { redisClient } from '../../../common/config/redis.config';
 import { createClient } from 'redis';
 import { RedisClientType } from 'redis';
+import { QueueStatusRequestDto, QueueStatusResDto } from '../../../presentation/dto/queue-status.dto';
+import { QueueStatusEnum } from '../../../common/enums/queue-status.enum'
 
 describe('QueueFacade', () => {
   let facade: QueueFacade;
@@ -24,12 +25,26 @@ describe('QueueFacade', () => {
   let dataSource: DataSource;
   let userRepository;
   const concertIdGlobal = uuid();
+  const userIdGlobal = uuid();
   let jwtService = new JwtService();
   let queueService;
   let concertService;
   let userService;
   let redisClient
   let redisRepository;
+
+  const mockUserService = {
+    getUserById: jest.fn(),
+  };
+
+  const mockConcertService = {
+    getConcertByQueueId: jest.fn(),
+  };
+
+  const mockJwtService = {
+    verifyToken: jest.fn(),
+    generateToken: jest.fn(),
+  };
 
   beforeAll(() => {
     jest.setTimeout(20000); // 전체 테스트 스위트에 대한 타임아웃을 20초로 설정
@@ -72,7 +87,7 @@ describe('QueueFacade', () => {
       username: 'postgres',
       password: 'password',
       database: 'concert_service_db',
-      entities: [Queue, Concert, User],
+      entities: [Concert, User],
       synchronize: true,
       extra: {
         max: 10, // 최대 커넥션 수를 10으로 설정
@@ -84,10 +99,6 @@ describe('QueueFacade', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         {
-          provide: getRepositoryToken(Queue),
-          useValue: dataSource.getRepository(Queue),
-        },
-        {
           provide: getRepositoryToken(Concert),
           useValue: dataSource.getRepository(Concert),
         },
@@ -97,25 +108,18 @@ describe('QueueFacade', () => {
         },
         {
           provide: QueueService,
-          useValue: new QueueService(
-            dataSource.getRepository(Queue),
-            dataSource.getRepository(Concert),
-            jwtService,
-            dataSource
-          ),
+          useValue: new QueueService(),
         },
         {
           provide: ConcertService,
           useValue: new ConcertService(
             dataSource.getRepository(Concert),
-            dataSource.getRepository(Queue),
           ),
         },
         {
           provide: UserService,
           useValue: new UserService(
             dataSource.getRepository(User),
-            dataSource.getRepository(Queue),
           ),
         },
         {
@@ -128,6 +132,10 @@ describe('QueueFacade', () => {
             redisRepository,
           ),
         },
+        {
+          provide: RedisRepository,
+          useValue: redisRepository,
+        },
         QueueFacade,
         JwtService,
       ],
@@ -135,23 +143,32 @@ describe('QueueFacade', () => {
 
     facade = module.get<QueueFacade>(QueueFacade);
     concertRepository = module.get(getRepositoryToken(Concert));
-    queueRepository = module.get(getRepositoryToken(Queue));
     userRepository = module.get(getRepositoryToken(User));
     jwtService = module.get(JwtService);
     queueService = module.get(QueueService);
     concertService = module.get(ConcertService);
     userService = module.get(UserService);
 
+    const now = new Date()
     // 콘서트 테이블에 테스트 데이터 삽입
     await concertRepository.save({
       id: concertIdGlobal,
       name: '테스트 콘서트',
       venue: '테스트 장소',
-      concert_date: new Date(), // 콘서트 날짜 설정
+      concert_date: now, // 콘서트 날짜 설정
       max_seats: 100, // 최대 좌석 수 100으로 설정
-      queue_count: 0, // 대기열 수를 0으로 시작
-      max_queue: 50, // 최대 대기열 수 제한 설정
+
+      reservation_start_time: now
     });
+
+    // 사용자 테이블에 테스트 데이터 삽입
+    await userRepository.save({
+      id: userIdGlobal, // UUID 생성
+      username: `user_${Math.random().toString(36).substring(2, 7)}`, // 랜덤한 사용자 이름
+      isAdmin: false, // 기본값 설정
+      balance: 0, // 기본값 설정
+    });
+
   });
 
   afterEach(async () => {
@@ -162,19 +179,10 @@ describe('QueueFacade', () => {
     }
   });
 
-  it('동시 요청을 처리하고 순차적인 대기열 위치를 할당해야 합니다 {예상 결과 : 동시에 100개 요청 시도 후 1 - 100 까지 모든 순서가 중복없이 부여}', async () => {
-    const reqAmount = 100;
-    const users = Array.from({ length: reqAmount }, () => ({
-      id: uuid(), // UUID 생성
-      username: `user_${Math.random().toString(36).substring(2, 7)}`, // 랜덤한 사용자 이름
-      isAdmin: false, // 기본값 설정
-      balance: 0, // 기본값 설정
-    }));
-
-    // 사용자 테이블에 테스트 데이터 삽입
-    await userRepository.save(users);
-
-    const createTokenDtos = users.map(user => ({ userId: user.id, concertId: concertIdGlobal }));
+  it('동시 요청을 처리하고 순차적인 대기열 위치를 할당해야 합니다 {예상 결과 : 동시에 1000개 요청 시도 후 1 - 1000 까지 모든 순서가 중복없이 부여}', async () => {
+    const reqAmount = 1000;
+    // const reqAmount = 10;
+    const createTokenDtos = Array.from({ length: reqAmount }, () => ({ userId: uuid(), concertId: concertIdGlobal }));
 
     // QueueFacade의 여러 인스턴스를 생성하고 동시에 호출
     const results = await Promise.all(createTokenDtos.map(dto => {
@@ -191,5 +199,88 @@ describe('QueueFacade', () => {
     const uniqueResults = new Set(results);
     expect(uniqueResults.size).toBe(reqAmount);
     expect(uniquePosition.size).toBe(reqAmount);
+  });
+
+  it('activeAt을 잘 배분하는지 확인합니다. 1000개의 요청이 있고 10초당 250명씩 받는다면 10초당 4번 250명씩 activeAt 시간을 배정받아야 합니다', async () => {
+
+    process.env.CYCLE_DURATION_MS = '10000'; // Set cycle to 10000 ms
+    process.env.MAX_PER_CYCLE = '250'; // Set maxPerCycle to 250
+
+    const reqAmount = 1000;
+
+    // const reqAmount = 10;
+    const createTokenDtos = Array.from({ length: reqAmount }, () => ({ userId: uuid(), concertId: concertIdGlobal }));
+
+    // QueueFacade의 여러 인스턴스를 생성하고 동시에 호출
+    const results = await Promise.all(createTokenDtos.map(dto => {
+      return facade.createToken(dto); // 서비스 메서드 호출
+    }));
+
+    const activeAtCount = {};
+    results.forEach(result => {
+      expect(result).toBeDefined();
+      const decodedResult = jwtService.decodeToken(result)
+      // activeAt을 기준으로 개수 카운트
+      const activeAtValue = decodedResult["activeAt"];
+      activeAtCount[activeAtValue] = (activeAtCount[activeAtValue] || 0) + 1;
+    });
+
+    // `activeAt` keys는 4사이클이니까 4입니다
+    expect(Object.keys(activeAtCount).length).toBe(4);
+
+    // 10초마다 각 값은 1000/4 = 250명씩 균일하게 배분되어야합니다
+    Object.values(activeAtCount).forEach(count => {
+      expect(count).toBe(250);
+    });
+  });
+
+
+
+  describe('getQueueStatus', () => {
+    it('activeAt 시점이 도래했다면 AVAILABLE을 리턴해야합니다', async () => {
+      const now = new Date();
+      const userId = userIdGlobal
+      const concertId = uuid()
+      const decodedToken = {
+        userId: userId,
+        concertId: concertId,
+        queuePosition: 1,
+        activeAt: new Date(now.getTime() - 1000 * 60), // 1 minute ago
+      };
+      const token = jwtService.generateToken(decodedToken)
+      const getQueueStatusDto: QueueStatusRequestDto = { token: token };
+
+      mockUserService.getUserById.mockResolvedValue({ id: userId });
+      mockConcertService.getConcertByQueueId.mockResolvedValue({ id: concertId });
+
+
+      const result = await facade.getQueueStatus(getQueueStatusDto);
+
+      expect(result.status).toBe(QueueStatusEnum.AVAILABLE);
+
+    });
+    it('activeAt 시점이 도래하지 않았다면 NOTAVAILABLE을 리턴해야합니다', async () => {
+      const now = new Date();
+      const userId = userIdGlobal
+      const concertId = uuid()
+      const decodedToken = {
+        userId: userId,
+        concertId: concertId,
+        queuePosition: 1,
+        activeAt: new Date(now.getTime() + 1000 * 60 * 10), // 10 minute later
+      };
+      const token = jwtService.generateToken(decodedToken)
+      const getQueueStatusDto: QueueStatusRequestDto = { token: token };
+
+      mockUserService.getUserById.mockResolvedValue({ id: userId });
+      mockConcertService.getConcertByQueueId.mockResolvedValue({ id: concertId });
+
+
+      const result = await facade.getQueueStatus(getQueueStatusDto);
+
+      expect(result.status).toBe(QueueStatusEnum.NOTAVAILABLE);
+
+    });
+   
   });
 });
